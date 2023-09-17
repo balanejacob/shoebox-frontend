3974967e5402a5ea44f354b2724ad6329ac3700c
Jacob Balane <balanejacob1@gmail.com>
2023-09-17 19:03:15 +0800

    added login and register buttons, logo, and category buttons

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,26 +1,33 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col cols="12">
-                <h1>Home Page</h1>
-            </v-col>
-        </v-row>
-        <nav>
-            <ul>
-                <li><router-link to="/">Home</router-link></li>
-                <li><router-link to="/login">Login</router-link></li>
-                <li><router-link to="/register">Register</router-link></li>
-                <li><router-link to="/shoe/:id">Shoe Detail</router-link></li>
-                <li><router-link to="/profile">Profile</router-link></li>
-                <li><router-link to="/cart">Cart</router-link></li>
-                <li><router-link to="/checkout">Checkout</router-link></li>
-                <li><router-link to="/order/:id">Order Detail</router-link></li>
-            </ul>
-        </nav>
+    <AppBar />
+
+    <v-container class="w-75 fill-height overflow-auto align-start">
+        <v-container class="pa-0">
+            <v-row class="my-6 align-center pa-0">
+                <v-col cols="3">
+                    <v-img
+                        :width="180"
+                        aspect-ratio="16/9"
+                        cover
+                        src="../../../public/logo.png"
+                    />
+                </v-col>
+                <v-col cols="6">
+                    <v-row class="justify-center">
+                        <v-btn class="mx-2" variant="text">men</v-btn>
+                        <v-btn class="mx-2" variant="text">female</v-btn>
+                        <v-btn class="mx-2" variant="text">kids</v-btn>
+                    </v-row>
+                </v-col>
+                <v-col cols="3">
+                    <!-- <v-text-field placeholder="Search" variant="solo-filled" class="no-shadow" v-bind:rounded="true"></v-text-field> -->
+                </v-col>
+            </v-row>
+            
+        </v-container>
     </v-container>
-    
 </template>
 
 <script setup>
-    //
+    import AppBar from '../AppBar.vue'
 </script>
