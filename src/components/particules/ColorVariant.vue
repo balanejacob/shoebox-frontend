e28473dc769078255eec6da143de9c8a8b8b802e
Jacob Balane <balanejacob1@gmail.com>
2023-10-15 17:41:54 +0800

    added shoelist UI in Homepage

---
+++ src/components/particules/ColorVariant.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="color-variant mr-2 my-1"
+    :style="{ backgroundColor: colorHex }"
+  ></div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  colorHex: String,
+});
+</script>
